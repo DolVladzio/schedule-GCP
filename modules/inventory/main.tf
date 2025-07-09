f41a92b2c869e1bcf1ec001a59d2b0730ef9384f
Vladzio <dolynkavladzio@gmail.com>
2025-07-09 15:02:28 +0000

    added a depends_on to the 'google_storage_bucket_object'

--- modules/inventory/main.tf
+++ modules/inventory/main.tf
@@ -9,5 +9,9 @@ resource "google_storage_bucket_object" "inventory_ini" {
   bucket = "ansible-inventory-file"
   source = "${path.module}/../../inventory/inventory.ini"
   content_type = "text/plain"
+
+  depends_on = [
+    local_file.ansible_inventory
+  ]
 }
 ##################################################################
