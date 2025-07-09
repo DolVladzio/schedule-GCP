ee520224e1a75df44b4f6a843a75441536789e29
Vladzio <dolynkavladzio@gmail.com>
2025-07-09 14:54:39 +0000

    changed code to store the inventory.ini by the Terraform

--- modules/inventory/main.tf
+++ modules/inventory/main.tf
@@ -4,3 +4,10 @@ resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../../inventory/inventory.ini"
 }
 ##################################################################
+resource "google_storage_bucket_object" "inventory_ini" {
+  name   = "inventory.ini"
+  bucket = "ansible-inventory-file"
+  source = "${path.module}/../../inventory/inventory.ini"
+  content_type = "text/plain"
+}
+##################################################################
