abed0a01d5d382fda0fab187e5473e9c78939ce6
Vladzio <dolynkavladzio@gmail.com>
2025-07-09 14:22:30 +0000

    changed a path to generate the 'inventory.ini'

--- modules/inventory/main.tf
+++ modules/inventory/main.tf
@@ -1,6 +1,6 @@
 ##################################################################
 resource "local_file" "ansible_inventory" {
   content  = var.inventory
-  filename = "${path.module}/../../../schedule-Ansible/inventory/inventory.ini"
+  filename = "${path.module}/../../inventory/inventory.ini"
 }
 ##################################################################
