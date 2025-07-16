09ea6430f35d211b4b9b15fd079449fc5762d1a4
Vladzio <dolynkavladzio@gmail.com>
2025-07-16 15:13:03 +0000

    update path for the json config

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "google_secret_manager_secret_version" "db_password" {
 }
 ##################################################################
 locals {
-  config = jsondecode(file("${path.module}/../schedule-config/terraform.json"))
+  config = jsondecode(file("${path.module}/../schedule-terraform-config/terraform.json"))
 
   fixed_region_map = {
     gcp = "europe-west3"
