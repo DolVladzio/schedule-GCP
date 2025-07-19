cca7aa295dcf1cec8c9fecca874ac12db9db5ed4
Vladzio <dolynkavladzio@gmail.com>
2025-07-19 16:02:29 +0000

    changed a path to the json config

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ data "google_secret_manager_secret_version" "db_password" {
 }
 ##################################################################
 locals {
-  config = jsondecode(file("${path.module}/terraform.json"))
+  config = jsondecode(file("${path.module}/schedule-terraform-config/terraform.json"))
 
   region = local.config.project.region[var.environment]
 
