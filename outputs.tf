1ddedae233c825a7b41f2ba1eeffbf7412d79cf8
Vladzio <dolynkavladzio@gmail.com>
2025-07-21 14:35:50 +0000

    deleted useless GKE outputs

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,3 @@
-### GKE cluster ##################################################
-output "gke_cluster_endpoints" {
-  description = "Map of GKE cluster names to their API endpoints"
-  value       = module.gke_cluster.cluster_endpoints
-}
-
-output "gke_cluster_ca_certificates" {
-  description = "Map of GKE cluster names to their CA certificates"
-  value       = module.gke_cluster.cluster_ca_certificates
-}
 ### Network ######################################################
 output "subnet_self_links_by_name" {
   description = "Map of subnets"
